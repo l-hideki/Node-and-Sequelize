a4da67ddd43a6df2ffe2febf7232f3973b0dbc54
unknown <lucas_hideki211@hotmail.com>
2020-06-23 19:10:19 -0300

    done

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const routes = express.Router();
 const userController = require('./controllers/userController');
+const addressController = require('./controllers/addressController')
+const techController = require('./controllers/techControllers');
+const reportController = require('./controllers/reportControllers');
 
-routes.post('/users',userController.store);
+const routes = express.Router();
+
+
+routes.get('/users', userController.index);
+routes.post('/users', userController.store);
+
+routes.post('/users/:user_id/addresses', addressController.store);
+routes.get('/users/:user_id/addresses', addressController.index);
+
+routes.post('/users/:user_id/techs', techController.store);
+routes.get('/users/:user_id/techs', techController.index);
+routes.delete('/users/:user_id/techs', techController.delete);
+
+routes.get('/report',reportController.show);
+
 
 module.exports = routes;
